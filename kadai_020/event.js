def5b9c98957aebf7026ee799e59dfc5a18bf515
IppeiSugiyama <kani.ng.kane.ok@gmail.com>
2025-01-21 15:10:57 +0900

    課題020提出

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+//btn要素を定数に代入
+const btnChenge = document.getElementById('btn');
+
+// text要素を定数に代入
+const textChenge = document.getElementById('text');
+
+// btn要素がクリックされたときにイベントを実行
+btnChenge.addEventListener('click',() => {
+  textChenge.textContent = 'ボタンをクリックしました'
+})
